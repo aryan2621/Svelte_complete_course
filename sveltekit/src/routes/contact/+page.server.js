ccb791fcb085025c1e37c664d81d86172081a706
Rishabh Verma <aryanverma2412@gmail.com>
2023-01-31 23:20:36 +0530

    Day-3 in svelte-kit

--- sveltekit/src/routes/contact/+page.server.js
+++ sveltekit/src/routes/contact/+page.server.js
@@ -1,3 +1,12 @@
+import { fail } from '@sveltejs/kit';
+import { object, string } from 'yup';
+
+const schema = object({
+	name: string().min(2).required('Please enter your name'),
+	email: string().email('Please enter a valid email').required('Please enter your email'),
+	message: string().required('Please enter your message')
+});
+
 export const actions = {
 	default: async ({ request }) => {
 		console.log('Contact form submitted!');
@@ -9,8 +18,53 @@ export const actions = {
 		console.log('Name:', name);
 		console.log('Email:', email);
 		console.log('Message:', message);
-		return {
-			message: 'Thank you for your message!'
-		};
+		if (!message) {
+			// return {
+			// 	error: 'Please enter your name'
+			// };
+			// return fail(400, { message: 'Please enter your name', email, name, status: 'error' });
+			// return fail(400, { error: 'Please enter your message', email, name, status: 'error' });
+			// to make value be there and not disappear and only
+			// missing vaule to be highlighted
+		}
+		if (!email) {
+			// return {
+			// 	error: 'Please enter your name'
+			// };
+			// return fail(400, { error: 'Please enter your email', message, name, status: 'error' });
+			// to make value be there and not disappear and only
+			// missing vaule to be highlighted
+		}
+		if (!name) {
+			// return {
+			// 	error: 'Please enter your name'
+			// };
+			// return fail(400, { error: 'Please enter your name', email, message, status: 'error' });
+			// to make value be there and not disappear and only
+			// missing vaule to be highlighted
+		}
+		try {
+			const result = await schema.validate({ name, email, message }, { abortEarly: false });
+			console.log('result', { result });
+			return {
+				success: true,
+				name,
+				email,
+				message
+			};
+		} catch (error) {
+			// console.log('error', { error });
+			const errors = error.inner.reduce((acc, curr) => {
+				acc[curr.path] = curr.message;
+				return acc;
+			}, {});
+			console.log('errors', { errors });
+			return {
+				errors,
+				name,
+				email,
+				message
+			};
+		}
 	}
 };
--- sveltekit/src/routes/contact/+page.svelte
+++ sveltekit/src/routes/contact/+page.svelte
@@ -1,34 +1,99 @@
 <script>
+	import { applyAction, enhance } from '$app/forms';
+	import { error } from '@sveltejs/kit';
 	export let form;
 	console.log(form);
 </script>
 
-<div class="container w-60 m-auto my-4">
-	<form method="POST">
-		<div class="form-group">
-			<label for="nameInput">Name</label>
-			<input
-				type="text"
-				class="form-control"
-				id="nameInput"
-				placeholder="Enter your name"
-				name="name"
-			/>
-		</div>
-		<div class="form-group">
-			<label for="emailInput">Email address</label>
-			<input
-				type="email"
-				class="form-control"
-				id="emailInput"
-				placeholder="Enter your email"
-				name="email"
-			/>
-		</div>
-		<div class="form-group">
-			<label for="messageInput">Message</label>
-			<textarea class="form-control" id="messageInput" rows="3" name="message" />
-		</div>
-		<button type="submit" class="btn btn-primary my-4 w-100">Submit</button>
-	</form>
-</div>
+{#if form?.success}
+	<div class="alert alert-info" role="alert">
+		{form.name}, your message has been sent. Thank you!
+	</div>
+{:else if form?.error}
+	<div class="alert alert-danger" role="alert">{form.error}</div>
+	<div class="container w-60 m-auto my-4">
+		<form
+			method="POST"
+			use:enhance={() => {
+				return async ({ result, error }) => {
+					await applyAction(result, error);
+				};
+			}}
+		>
+			<div class="form-group">
+				<label for="nameInput">Name</label>
+				<input
+					type="text"
+					class="form-control"
+					id="nameInput"
+					placeholder="Enter your name"
+					name="name"
+					value={form?.name || ''}
+					class:error={form?.errors?.name}
+				/>
+			</div>
+			<div class="form-group">
+				<label for="emailInput">Email address</label>
+				<input
+					type="email"
+					class="form-control"
+					id="emailInput"
+					placeholder="Enter your email"
+					name="email"
+					value={form?.email || ''}
+					class:error={form?.errors?.email}
+				/>
+			</div>
+			<div class="form-group">
+				<label for="messageInput">Message</label>
+				<textarea
+					class="form-control"
+					value={form?.message || ''}
+					id="messageInput"
+					rows="3"
+					name="message"
+					class:error={form?.errors?.message}
+				/>
+			</div>
+			<button type="submit" class="btn btn-primary my-4 w-100">Submit</button>
+		</form>
+	</div>
+{:else}
+	<div class="container w-60 m-auto my-4">
+		<form method="POST" use:enhance>
+			<div class="form-group">
+				<label for="nameInput">Name</label>
+				<input
+					type="text"
+					class="form-control"
+					id="nameInput"
+					placeholder="Enter your name"
+					name="name"
+					value={form?.name || ''}
+				/>
+			</div>
+			<div class="form-group">
+				<label for="emailInput">Email address</label>
+				<input
+					type="email"
+					class="form-control"
+					id="emailInput"
+					placeholder="Enter your email"
+					name="email"
+					value={form?.email || ''}
+				/>
+			</div>
+			<div class="form-group">
+				<label for="messageInput">Message</label>
+				<textarea
+					class="form-control"
+					value={form?.message || ''}
+					id="messageInput"
+					rows="3"
+					name="message"
+				/>
+			</div>
+			<button type="submit" class="btn btn-primary my-4 w-100">Submit</button>
+		</form>
+	</div>
+{/if}
